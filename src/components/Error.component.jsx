4df0b1ae0c2165b8ca7e6dad855356c9c8b38121
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-07-13 22:38:26 +0530

    fix: fix padding in error components

--- src/components/Error.component.jsx
+++ src/components/Error.component.jsx
@@ -5,7 +5,7 @@ import { House } from "lucide-react";
 export const ErrorComponent = () => {
   const navigate = useNavigate();
   return (
-    <main className="grid min-h-[100svh] place-items-center content-center items-center gap-8 p-8">
+    <main className="grid min-h-[100svh] place-items-center content-center items-center gap-6 p-8">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
         404 | Page Not Found
       </h1>
